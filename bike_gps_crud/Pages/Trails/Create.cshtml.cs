170a87f6ac91949410cd5ce6172175bb1cb12d38
Josh Aney <josh.aney@icloud.com>
2025-02-06 15:37:55 -0700

    Working Images

--- bike_gps_crud/Pages/Trails/Create.cshtml.cs
+++ bike_gps_crud/Pages/Trails/Create.cshtml.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using bike_gps_crud.Data;
 using bike_gps_crud.Models;
 
@@ -12,11 +11,14 @@ namespace bike_gps_crud.Pages.Trails
 {
     public class CreateModel : PageModel
     {
-        private readonly bike_gps_crud.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly string storagePath;
 
-        public CreateModel(bike_gps_crud.Data.ApplicationDbContext context)
+        public CreateModel(ApplicationDbContext context)
         {
             _context = context;
+            // Store images in wwwroot/Trail-Images
+            storagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Trail-Images");
         }
 
         public IActionResult OnGet()
@@ -27,13 +29,38 @@ namespace bike_gps_crud.Pages.Trails
         [BindProperty]
         public Trail Trail { get; set; } = null!;
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+        [BindProperty]
+        public IFormFile TrailImage { get; set; } = null!;
+
+        public string Message { get; set; } = "";
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            if (TrailImage == null || TrailImage.Length == 0)
+            {
+                Message = "No file selected.";
+                return Page();
+            }
+
+            if (!Directory.Exists(storagePath))
+            {
+                Directory.CreateDirectory(storagePath);
+            }
+
+            var fileName = $"{Guid.NewGuid()}.png";
+            var filePath = Path.Combine(storagePath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await TrailImage.CopyToAsync(stream);
+            }
+
+            Trail.ImageUrl = $"Trail-Images/{fileName}";
 
             _context.Trail.Add(Trail);
             await _context.SaveChangesAsync();
