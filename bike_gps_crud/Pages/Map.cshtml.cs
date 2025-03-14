b73a9763c3174cc4f155a6edebecbdec8bfe92ba
Josh Aney <josh.aney@icloud.com>
2025-03-14 12:22:41 -0600

    Start MapBox

---
+++ bike_gps_crud/Pages/Map.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace bike_gps_crud.Pages;
+
+public class MapModel : PageModel
+{
+    public string MapboxAccessToken { get; } = "pk.eyJ1Ijoiam9zaGFuZXkzMjQiLCJhIjoiY204OTJtZDl3MHdxajJqcHMza2R2bHRyOCJ9.UgXb_yFl0DtJVaFrJ0qRxw";
+
+    public void OnGet()
+    {
+    }
+}
