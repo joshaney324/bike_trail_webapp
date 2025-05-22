d8ccbc5e416770269d57c1be277d7c855fd5a451
joshaney324 <josh.aney@icloud.com>
2025-05-22 12:58:19 -0500

    update trails repo page to only include user's trails

--- bike_gps_crud/Pages/Trails/Index.cshtml.cs
+++ bike_gps_crud/Pages/Trails/Index.cshtml.cs
@@ -5,16 +5,20 @@ using bike_gps_crud.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
 
 namespace bike_gps_crud.Pages.Trails
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
 
-        public IndexModel(ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Trail> Trails { get; set; } = new List<Trail>();
@@ -27,14 +31,25 @@ namespace bike_gps_crud.Pages.Trails
         [BindProperty(SupportsGet = true)]
         public string? TrailType { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            IQueryable<string> trailTypeQuery = from m in _context.Trail
-                orderby m.TrailType
-                select m.TrailType;
+            var user = await _userManager.GetUserAsync(User);
             
-            var trailsQuery = from t in _context.Trail
-                select t;
+            if (user == null)
+            {
+                return Challenge(); // force login
+            }
+
+            var userId = user.Id;
+            
+            
+            IQueryable<string> trailTypeQuery = _context.Trail
+                .Where(t => t.UserId == userId)
+                .OrderBy(t => t.TrailType)
+                .Select(t => t.TrailType);
+            
+            var trailsQuery = _context.Trail
+                .Where(t => t.UserId == userId);
             
             if (!string.IsNullOrEmpty(SearchString))
             {
@@ -49,6 +64,7 @@ namespace bike_gps_crud.Pages.Trails
             TrailTypes = new SelectList(await trailTypeQuery.Distinct().ToListAsync());
             Trails = await trailsQuery.ToListAsync();
             
+            return Page();
             
         }
     }
